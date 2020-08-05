1507abef38baf20576768e13dfa6843ac25aa753
SultanAlyahya <38963523+SultanAlyahya@users.noreply.github.com>
2020-08-05 17:37:49 +0300

    help screen

--- src/Screens/HelpScreen.js
+++ src/Screens/HelpScreen.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
-import {View, TouchableOpacity, Text, TextInput, ScrollView} from 'react-native'
+import {View, TouchableOpacity, Text, TextInput, ScrollView, FlatList, Platform} from 'react-native'
 import {socket} from '../socket/socketIo'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const Help =()=> {
@@ -8,49 +9,70 @@ const Help =()=> {
     const [message, setMessage] = useState('')
     const [chat, setChat] = useState([])
 
-    
+    const socketId = socket.id
     
     useEffect(()=>{
-        socket.removeEventListener("message")
+        socket.removeEventListener()
+        socket.on('userManagement', (newMessage)=>{
+            console.log(newMessage)
+        })
         socket.on('message', (newMessage)=>{
             if(newMessage.message === '')
-                return
-            const newChat = chat
-            newChat.push({message: newMessage.message, id: newMessage.id})
-            setChat([...newChat])
+                return;
+            setChat(chat => [...chat, newMessage])
         })
     },[])
 
 
     const send =()=> {
-        socket.emit('message', message)
+        if(message === '')
+            return;
+        const newMessage = {message, messageId: chat.length+"",delivered: false, userId: socketId}
+        setChat(chat => [...chat, newMessage])
         setMessage('')
+        socket.emit('message', newMessage, (messageId)=>{
+            console.log('the message has delivered')
+            let newChat = chat
+            newChat[messageId] = {...newMessage, delivered: true}
+            console.log(newChat)
+            setChat(newChat)        
+        })
     }
 
     return(
         <View style={{flex:1, backgroundColor: '#ededed'}}>
-            <ScrollView style={{flex:1}}>
 
-            {chat.map((textMessage, index)=>(
-                
-                <View key={index+''+textMessage.message} style={{backgroundColor: '#fff', margin: 2, padding: 10,
+            <FlatList
+            data={chat}
+            renderItem={({item})=>(
+                <View style={{backgroundColor: '#fff', margin: 2, padding: 10,
                 borderRadius: 10}}>
-                    {textMessage.id === socket.id?
-                    <Text style={{color: '#3ba8e7'}}>me</Text>
+                    {item.userId === socketId?
+                    <View style={{flexDirection: 'row'}}>
+                        <Text style={{color: '#3ba8e7'}}>me </Text>
+                        {item.delivered === true?
+                        <MaterialCommunityIcons name="check-all" size={18} color="black" />
+                        :
+                        <MaterialCommunityIcons name="check" size={18} color="black" />
+                        }
+                    </View>
                     :
                     <Text style={{color: '#33dd33'}}>assistant</Text>
                     }
-                    <Text key={index+''+textMessage.message} style={{fontSize: 20}}>{textMessage.message}</Text>
+                    <Text style={{fontSize: 20}}>{item.message}</Text>
                 </View>
-                
-            ))}
+            )}
+            keyExtractor={item=> item.messageId}
+            />
 
-            </ScrollView>
             <View style={{backgroundColor: '#c3c3c3', flexDirection: 'row', paddingHorizontal: 20, paddingBottom:30, paddingTop: 15}}>
                 <TextInput
-                style={{flex: 1, borderWidth: 1, borderColor: '#000', borderRadius: 10, backgroundColor: '#fff', height: 50, fontSize: 25}}
+                style={{flex: 1, borderWidth: 1, borderColor: '#000', borderRadius: 10, backgroundColor: '#fff', fontSize: 25, padding:5}}
                 onChangeText={(text)=> setMessage(text)}
                 value={message}
+                multiline={true}
+                numberOfLines={Platform.OS ==='ios'? null: 40}
+                maxHeight={Platform.OS === 'ios'  ? 160 : null}
                 />
                 <TouchableOpacity style={{justifyContent:'center'}}
                 onPress={()=> send()}>
